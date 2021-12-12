f1256303101ae6eb7cfcbb17206520d771d86679
Joemon V Velassery <joemonv97@gmail.com>
2021-12-12 16:09:54 +0530

    DB Register

--- webpage/views.py
+++ webpage/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import title
+from django.contrib.auth.models import User,auth
 def index(request):
     return render(request,"home.html")
 def register(request):
@@ -18,7 +19,15 @@ def login_user(request):
     return render(request,"article.html",{'result':a})
 
 def register_user(request):
-    a=request.POST["username"]
-    b=request.POST["password"]
-
-    return render(request,"article.html",{'result':a})
+    if request.method=="POST":
+        a=request.POST["first"]
+        b=request.POST["last"]
+        c=request.POST["user"]
+        d=request.POST["email"]
+        e=request.POST["password"]
+        f=request.POST["repeat_password"]
+        user=User.objects.create_user(username=c,password=e,last_name=b,email=d,first_name=a)
+        user.save()
+        return redirect("/article")
+    else:
+        return render(request,"register.html")
